e485c555bad6e296974bb48ced9eff46dd129c2b
Niklas Ovnell <niklas_ovnell@hotmail.com>
2017-05-01 15:59:37 +0200

    Mm
    lÃ¤ttare att ange storlek p delarna
    gjort anonyma klasser till vanliga klasser

--- src/gui/ChatClientGUI.java
+++ src/gui/ChatClientGUI.java
@@ -1,6 +1,5 @@
 package gui;
 
-import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
@@ -15,6 +14,7 @@ import java.io.OutputStreamWriter;
 import java.net.Socket;
 import java.util.LinkedList;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -22,96 +22,72 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 public class ChatClientGUI {
-	
+
 	public static void main(String[] args) {
 		new ChatClientGUI("localhost", 30000);
 	}
-	
+
+	// Kopplingen till servern
 	private Socket s;
 	private BufferedWriter writer;
 	private BufferedReader reader;
-	private JFrame frame = new JFrame("Chat"); // Fönstret
 
+	// Fönstret
+	private JFrame frame = new JFrame("Chat");
 	private JPanel mainPanel = new JPanel();
 	private JTextArea messages = new JTextArea();
-	
 	private JTextField textField2 = new JTextField();
+	
+	// Knappar
 	private JPanel buttonPanel = new JPanel();
 	private JButton broadcastButton = new JButton("Broadcast");
 	private JButton echoButton = new JButton("Echo");
 	private JButton quitButton = new JButton("Quit");
 
-	private Thread readThread;
+	private Thread readThread; // Tråd som läser input från servern.
 
 	public ChatClientGUI(String host, int port) {
 		quitButton.addActionListener(new QuitButtonListener());
-
-		buttonPanel.setLayout(new GridLayout(1, 0));
+		broadcastButton.addActionListener(new BroadcastButtonListener());
+		echoButton.addActionListener(new EchoButtonListener());
+		
+		buttonPanel.setLayout(new GridLayout(1, 0));	// Knapparna ligger på samma rad
 		buttonPanel.add(broadcastButton);
 		buttonPanel.add(echoButton);
 		buttonPanel.add(quitButton);
 
-		textField2.setText("text2");
-		mainPanel.setLayout(null);
+		textField2.setText("...");
+		messages.setPreferredSize(new Dimension(700, 425));
+		textField2.setPreferredSize(new Dimension(700, 150));
+		buttonPanel.setPreferredSize(new Dimension(700, 100));
+		mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.PAGE_AXIS));	// Lägger dem under varandra
 		mainPanel.add(messages);
 		mainPanel.add(textField2);
 		mainPanel.add(buttonPanel);
-		messages.setBounds(0, 0, 700, 425);
-		textField2.setBounds(0,425,700,150);
-		buttonPanel.setBounds(0, 575, 700, 100);
 
-		frame.getContentPane().add(mainPanel, BorderLayout.CENTER);
-		frame.setSize(new Dimension(700, 700));
-		frame.setLocationRelativeTo(null); // Gör så att fönstret hamnar mitt på skärmen
+		frame.add(mainPanel);
+		frame.pack();
+		frame.setLocationRelativeTo(null); // Gör så att fönstret hamnar mitt på
+											// skärmen
+		frame.setResizable(true);
 		frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-		frame.setVisible(true);
-		
 		// Gör så att fönstret anropar quit() när det stängs.
-		frame.addWindowListener(new WindowAdapter() {
-			@Override
-			public void windowClosing(WindowEvent e) {
-				quit();
-			}
-		});
-		
+		frame.addWindowListener(new WindowCloser());
+		frame.setVisible(true);
+
 		try {
 			s = new Socket(host, port);
 			writer = new BufferedWriter(new OutputStreamWriter(s.getOutputStream()));
 			reader = new BufferedReader(new InputStreamReader(s.getInputStream()));
-			quitButton.addActionListener(new QuitButtonListener());
-			broadcastButton.addActionListener(new BroadcastButtonListener());
-			echoButton.addActionListener(new EchoButtonListener());
-
-			readThread = new Thread() {
-				private LinkedList<String> messageList = new LinkedList<String>();
-				public void run() {
-					while (true) {
-						try {
-							String line = reader.readLine();
-							if (line != null) {
-								if (messageList.size() == 26) {
-									messageList.removeFirst();
-								}
-								messageList.addLast(line);
-								StringBuilder displayText = new StringBuilder();
-								for (String message : messageList) {
-									displayText.append(message + "\n");
-								}
-								messages.setText(displayText.toString());
-							}
-						} catch (IOException e) {
-							System.exit(0);
-						}
-					}
-				}
-			};
-			readThread.start();
 		} catch (Exception e) {
 			e.printStackTrace();
 			System.exit(1);
 		}
+		
+		readThread = new InputReaderThread();
+		readThread.start();
 	}
-	
+
 	private void sendMessage(String mess) {
 		try {
 			writer.write(mess + "\r\n");
@@ -121,7 +97,7 @@ public class ChatClientGUI {
 			System.exit(1);
 		}
 	}
-	
+
 	private void quit() {
 		try {
 			sendMessage("Q");
@@ -142,22 +118,66 @@ public class ChatClientGUI {
 		}
 	}
 
-	private class BroadcastButtonListener implements ActionListener {
+	private class MessageSendButton {
+
+		public void send(String prefix, String message) {
+			if (!message.isEmpty()) { // Skicka inte tomma meddelanden.
+				sendMessage(prefix + message);
+				textField2.setText("");
+			}
+		}
+	}
+
+	private class BroadcastButtonListener extends MessageSendButton implements ActionListener {
 
 		@Override
-		public void actionPerformed(ActionEvent arg0) {
-			sendMessage("M:" + textField2.getText());
+		public void actionPerformed(ActionEvent e) {
+			send("M:", textField2.getText());
 		}
 
 	}
 
-	private class EchoButtonListener implements ActionListener {
+	private class EchoButtonListener extends MessageSendButton implements ActionListener {
 
 		@Override
-		public void actionPerformed(ActionEvent arg0) {
-			sendMessage("E:" + textField2.getText());
+		public void actionPerformed(ActionEvent e) {
+			send("E:", textField2.getText());
 		}
 
+	}
+
+	private class WindowCloser extends WindowAdapter {
+
+		@Override
+		public void windowClosing(WindowEvent e) {
+			quit();
+		}
+	}
+
+	// Tråd som läser input från servern.
+	private class InputReaderThread extends Thread {
+		private LinkedList<String> messageList = new LinkedList<String>();
+
+		public void run() {
+			while (true) {
+				try {
+					String line = reader.readLine();
+					if (line != null) {
+						if (messageList.size() == 26) {
+							messageList.removeFirst();
+						}
+						messageList.addLast(line);
+						StringBuilder displayText = new StringBuilder();
+						for (String message : messageList) {
+							displayText.append(message + "\n");
+						}
+						messages.setText(displayText.toString());
+					}
+				} catch (IOException e) {
+					System.exit(0);
+				}
+			}
+		}
 	}
 
 }
